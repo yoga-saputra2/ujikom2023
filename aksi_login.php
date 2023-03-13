2ce79c70e5eeda90fa72fbac8a7b8f136ff117b4
yoga-saputra2 <127284751+yoga-saputra2@users.noreply.github.com>
2023-03-13 13:19:21 +0700

    Add files via upload

---
+++ aksi_login.php
@@ -0,0 +1,42 @@
+
+<?php 
+include 'conn/koneksi.php';
+	if(isset($_POST['login'])){
+		$username = mysqli_real_escape_string($koneksi,$_POST['username']);
+		$password = mysqli_real_escape_string($koneksi,md5($_POST['password']));
+	
+		$sql = mysqli_query($koneksi,"SELECT * FROM masyarakat WHERE username='$username' AND password='$password' ");
+		$cek = mysqli_num_rows($sql);
+		$data = mysqli_fetch_assoc($sql);
+	
+		$sql2 = mysqli_query($koneksi,"SELECT * FROM petugas WHERE username='$username' AND password='$password' ");
+		$cek2 = mysqli_num_rows($sql2);
+		$data2 = mysqli_fetch_assoc($sql2);
+
+		if($cek>0){
+			session_start();
+			$_SESSION['username']=$username;
+			$_SESSION['data']=$data;
+			$_SESSION['level']='masyarakat';
+			header('location:masyarakat/');
+		}
+		elseif($cek2>0){
+			if($data2['level']=="admin"){
+				session_start();
+				$_SESSION['username']=$username;
+				$_SESSION['data']=$data2;
+				header('location:admin/');
+			}
+			elseif($data2['level']=="petugas"){
+				session_start();
+				$_SESSION['username']=$username;
+				$_SESSION['data']=$data2;
+				header('location:petugas/');
+			}
+		}
+		else{
+			echo "<script>alert('Username atau Password Salah')</script>";
+		}
+
+	}
+ ?>
